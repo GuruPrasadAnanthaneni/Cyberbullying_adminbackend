69edc0afa9661709c13beb18beb0bf361a357262
GuruPrasadAnanthaneni <guruprasad.ananthaneni@gmail.com>
2024-12-17 15:26:32 +0530

    Final

--- index.js
+++ index.js
@@ -16,7 +16,28 @@ db.on("open", () => {
 db.on("error", () => {
   console.log("Database not Connected");
 });
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://cyberbullying-frontend.vercel.app/","https://cyberbullying-adminfrontend.vercel.app/"
+];
 
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin (e.g., mobile apps, Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(cors());
 app.use("/Signup-Login", SL);
